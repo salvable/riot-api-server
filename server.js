1796ff3489edd4d6f8169596ba86f04eca71772f
salvable <salvable@kakao.com>
2021-10-12 18:17:26 +0900

    docker 이미지 생성 , contaner

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const app = express();
+const server = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+server.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () =>
+server.listen(port, () =>
     console.log(`Example app listening at http://localhost:${port}`)
 );
