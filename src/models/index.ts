dd286d4de64ecc80851aa3667b2abfcf32676c74
salvable <salvable@kakao.com>
2021-10-26 19:18:15 +0900

    주석추가

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,8 @@ import {Sequelize} from 'sequelize-typescript';
 import {config} from '../config/config';
 import {User} from "./user";
 
+// models에 작성한 모델 추가해줌
+
 export const sequelize =  new Sequelize({
     database: config.development.database || "test",
     username: config.development.username || "root",
