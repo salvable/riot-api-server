f2173d3ab8249bdc60a8eb191702fe0c4cd4601e
salvable <salvable@kakao.com>
2021-10-27 19:57:31 +0900

    error handling 추가

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import * as express from "express";
 import * as dotenv from "dotenv";
 import * as cors from "cors";
 import {sequelize} from "./models";
+import errorMiddleware from './middleware/error.middleware';
+import HttpException from "./exceptions/HttpException";
 
 dotenv.config();
 
@@ -13,12 +15,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(errorMiddleware);
 
-app.get('/get', (req: express.Request, res: express.Response) => {
+app.get('/get', (req: express.Request, res: express.Response,next: express.NextFunction) => {
+    // 에러처리 코드
+    throw new HttpException(404, 'Post not found');
     res.send('Hello world');
 })
 
 const driver = async () =>{
+
     try{
         await sequelize.sync({force:false});
     } catch (err) {
--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
         password : process.env.DB_PASSWORD,
         database : process.env.DB_DBNAME || 'typescript_test',
         host : process.env.DB_HOST || 'localhost',
-        port : process.env.DB_PORT || 3000,
+        port : process.env.DB_PORT || 3306,
         dialect : "mysql"
     }
 }
