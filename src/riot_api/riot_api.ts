b3e280ccbcbb6e916b1bcc2f1c7fbf362df50d87
salvable <salvable@kakao.com>
2021-11-23 21:24:32 +0900

    한글은 api 요청이 안되던 점 수정

--- src/riot_api/riot_api.ts
+++ src/riot_api/riot_api.ts
@@ -1,10 +1,10 @@
 import * as express from "express";
 import {getEnNameByChampionId, getKrNameByChampionId} from "../util/ riot_ChampionIdToName"
+const encoding = require('urlencode');
 const axios = require('axios');
 
 export const getSummoner= async (req: express.Request, res:express.Response, next:express.NextFunction)=> {
-    const summonerName = req.params.summonerName
-
+    const summonerName = encoding(req.params.summonerName)
     const summoner = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.RIOT_API_KEY}`)
 
     return res.send(
